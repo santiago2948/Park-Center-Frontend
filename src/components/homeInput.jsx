26da9940a362d5d7b72a62e3331e436a36a27602
Santiago <rasua2948@gmail.com>
2026-01-03 12:03:51 -0500

    Feat: login for parking admins created

--- src/components/homeInput.jsx
+++ src/components/homeInput.jsx
@@ -1,12 +1,14 @@
 import styles from '../pages/home/home.module.css';
 
-export function HomeInput({ value, onChange, placeholder, type = 'text' }) {
+export function HomeInput({ value, onChange, placeholder, type = 'text', name }) {
     return (
         <input
+            required
             value={value}
             onChange={onChange}
             className={`form-control mb-3 ${styles.placaInput}`}
             type={type}
+            name={name}
             placeholder={placeholder}
         />
     );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,6 +5,9 @@ import {Logo} from '../../components/Logo.jsx';
 
 // Imports from React
 import {useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+
 
 export function HomeScreen() {
     useEffect(() => {
@@ -15,24 +18,59 @@ export function HomeScreen() {
     }, []);
 
     const [placa, setPlaca] = useState("");
+    const [login, setLogin] = useState(false);
 
     const placaFormat = (e) =>{
         let value = e.target.value.toUpperCase();
         if(value.length <= 6) setPlaca(value);
     };
 
-    const redirectAdminPage = () => {
-        window.location.href = '/admin';
+    const changeToLogin = () => {
+        setLogin(login => !login);
+    }
+
+    // Login logic would go below here
+    const [loginState, setLoginState] = useState(0); // 0: email, 1: password
+    const [loginForm, setLoginForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setLoginForm({
+            ...loginForm,
+            [e.target.name]: e.target.value
+        });
     }
 
     return (
             <div className={styles.homeManBox}>
                 <Logo />
-                <form action="#">
-                    <HomeInput value={placa} onChange={placaFormat} placeholder='Placa' />
-                    <SubmitButton text="Continuar" className="mb-3 w-100"/>
-                </form>
-                <p onClick={redirectAdminPage} className={styles.adminQuestion}>¿Eres administrador?</p>
+                {
+                    login ? 
+                         <form action="#">
+                            {loginState === 0 ?
+                            <>
+                                <HomeInput value={loginForm.email} onChange={handleChange} placeholder='Email' type='email' name='email' />
+                                <SubmitButton text="Siguiente" onClick={(e)=> setLoginState(1)} className="mb-3 w-100"/>
+                            </>
+                            :<>
+                                <p><span className={styles.editEmailIcon}><FontAwesomeIcon icon={faPenToSquare} style={{marginRight: "6px"}} onClick={(e) => setLoginState(0)}/></span> Email: {loginForm.email}</p>
+                                <HomeInput value={loginForm.password} onChange={handleChange} placeholder='Contraseña' type='password' name='password' />
+                                <SubmitButton text="Ingresar" type='submit' className="mb-3 w-100"/>
+                            </>    
+                            }
+                            <p onClick={changeToLogin} className={styles.adminQuestion}>Pagar por placa</p>
+                        </form>
+                    :<> 
+                        <form action="#">
+                            <HomeInput value={placa} onChange={placaFormat} placeholder='Placa' />
+                            <SubmitButton type='submit' text="Continuar" className="mb-3 w-100"/>
+                            <p onClick={changeToLogin} className={styles.adminQuestion}>¿Eres administrador?</p>
+                        </form>
+                    </> 
+                }
+                
             </div>
         )
 }
